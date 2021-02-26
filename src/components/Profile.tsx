c96805b91beae53e201fbd4cb570099c5b66c472
Francisco Morais <fgmorais18@gmail.com>
2021-02-26 14:24:36 -0300

    add storage (cookie) and deploy

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+   const { level } = useContext(ChallengesContext);
+   
    return(
       <div className={styles.profileContainer}>
          <img src="https://github.com/franmoraiiss.png" alt="Francisco Morais"/>
@@ -8,7 +12,7 @@ export function Profile() {
             <strong>Francisco Morais</strong>
             <p>
                <img src="icons/level.svg" alt="Level"/>
-               Level 1
+               Level {level}
             </p>
          </div>
       </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,11 @@
-import { useState } from 'react';
-
 import '../styles/global.css'
 
 import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 function MyApp({ Component, pageProps }) {
   
-
   return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
+    <Component {...pageProps} />
   )
 }
 
